abd00c3665bbc7c18a013596049eea6e42fa5260
Stefan <stefan.kubik.pl@gmail.com>
2026-01-05 12:02:22 +0100

    Phase 3: Integrate config system with ModMenu
    - Updated ModMenuIntegration to return SignScribeConfig screen
    - Enables configuration access through ModMenu's settings UI
    - Users can now configure mod behavior through ModMenu interface
    - Links ModMenu config entry point to SignScribeConfig.getConfigScreen()

--- src/main/java/com/signscribe/integration/ModMenuIntegration.java
+++ src/main/java/com/signscribe/integration/ModMenuIntegration.java
@@ -2,10 +2,11 @@ package com.signscribe.integration;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import com.signscribe.SignScribeConfig;
 
 public class ModMenuIntegration implements ModMenuApi {
 	@Override
 	public ConfigScreenFactory<?> getModConfigScreenFactory() {
-		return parent -> null;
+		return parent -> SignScribeConfig.getInstance().getConfigScreen(parent);
 	}
 }
